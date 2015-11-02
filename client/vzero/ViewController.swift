f6053ffc64d941b178042cf8ee480866c93cd9a1
Philippe Tsakyrellis <philippe@blitsy.com>
2015-11-01 18:55:17 -0600

    swift 2 and xcode 7.0 compat

--- client/vzero/ViewController.swift
+++ client/vzero/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController, BTDropInViewControllerDelegate {
     }
 
     @IBAction func payClick(sender: AnyObject) {
-        var dropInViewController: BTDropInViewController = braintree!.dropInViewControllerWithDelegate(self)
+        let dropInViewController: BTDropInViewController = braintree!.dropInViewControllerWithDelegate(self)
         dropInViewController.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Cancel, target: self, action: Selector("userDidCancel"))
         
         //Customize the UI
@@ -30,7 +30,7 @@ class ViewController: UIViewController, BTDropInViewControllerDelegate {
         dropInViewController.summaryDescription = "Enough for a good cup of coffee"
         dropInViewController.displayAmount = "$10"
 
-        var navigationController: UINavigationController = UINavigationController(rootViewController: dropInViewController)
+        let navigationController: UINavigationController = UINavigationController(rootViewController: dropInViewController)
 
         self.presentViewController(navigationController, animated: true, completion: nil)
     }
@@ -39,24 +39,24 @@ class ViewController: UIViewController, BTDropInViewControllerDelegate {
         manager.GET("\(serverBase)/token",
             parameters: nil,
             success: { (operation: AFHTTPRequestOperation!, responseObject: AnyObject!) -> Void in
-                var clientToken = responseObject["clientToken"] as String
+                let clientToken = responseObject["clientToken"] as! String
                 self.braintree = Braintree(clientToken: clientToken)
                 self.payButton.userInteractionEnabled = true
             }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
-                println(error.description)
+               print(error.description)
         }
     }
 
     func postNonce(paymentMethodNonce: String) {
-        var parameters = ["payment_method_nonce": paymentMethodNonce]
+        let parameters = ["payment_method_nonce": paymentMethodNonce]
 
         manager.POST("\(serverBase)/payment",
             parameters: parameters,
             success: { (operation: AFHTTPRequestOperation!, responseObject: AnyObject!) -> Void in
-                var transactionId : String = (responseObject["transaction"] as AnyObject!)["id"] as String
+                let transactionId : String = (responseObject["transaction"] as AnyObject!)["id"] as! String
                 self.transactionLabel.text = "Transaction ID: \(transactionId)"
             }) { (operation: AFHTTPRequestOperation!, error: NSError!) -> Void in
-                println(error.description)
+                print(error.description)
         }
     }
 
